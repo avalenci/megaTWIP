d9601232eff2ebb4871ea1b0e334494c12b79fa3
avalenci <ansmeew@gmail.com>
2017-05-07 18:14:02 -0400

    It works I think!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,17 +17,38 @@ public class Main {
         ArrayList<String> lines = new ArrayList<>();
         rawData.nextLine();
         while (rawData.hasNext()) {
-            lines.add(rawData.nextLine());
-        }
-        for (String x : lines) {
-            Scanner s = new Scanner(x);
+            Scanner s = new Scanner(rawData.nextLine());
             s.useDelimiter("\t");
             Students a = findStudent(s.nextInt());
             double q1 = 0, q2 = 0;
+            while (s.hasNext()) {
+                double receive = getGrade(s.next());
+                if (receive == .01 && !(s.hasNextInt()))
+                    break;
+                else if (receive < 0) {
+                    q1 += receive;
+                    break;
+                }
+                else if (receive != .01) {
+                    q1 += receive;
+                }
+            }
+            while (s.hasNext()) {
+                double receive = getGrade(s.next());
+                if (receive < 0) {
+                    q2 += receive;
+                    break;
+                }
+                else if (receive != .01) {
+                    q2 += receive;
+                }
+            }
             a.setGrades(q1, q2);
         }
+        System.out.println("Name\tSecret Number\tAvg Total Grade\tFR Q1 Grade\tFR Q2 Grade");
         for (Students a: student) {
-            //System.out.println(a.getSecretNumber() + " " + a.totalGrade + " " + a.q1Grade + " " + a.q2Grade);
+            System.out.println(a.getStudentName() + "\t" + a.getSecretNumber() + "\t\t\t\t"
+                    + a.totalGrade + "\t\t\t\t" + a.q1Grade + "\t\t\t" + a.q2Grade);
         }
     }
     static Students findStudent(int numb) {
@@ -39,6 +60,28 @@ public class Main {
     }
     static double getGrade(String s) {
         double points = 0;
+        if (s.equals("") || s.equals("0"))
+            return .01;
+        try {
+            points -= Integer.parseInt(s) * .25;
+            return points;
+        }
+        catch (Exception e) {}
+        String grade[] = s.split(", ");
+        for(String l : grade)
+        {
+            Scanner get = new Scanner(l);
+            get.next();
+            try
+            {
+                points += Double.parseDouble(get.next());
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+
         return points;
     }
     static ArrayList<Students> student = new ArrayList<>();
--- src/com/company/Students.java
+++ src/com/company/Students.java
@@ -12,19 +12,15 @@ public class Students {
         return studentName;
     }
     public void setGrades(double q1, double q2) {
-        q1numb++;
-        q2numb++;
-        q1Grade += (int)(q1/q1numb);
-        q2Grade += (int)(q2/q2numb);
-        if (q2numb == 2) {
-            totalGrade = (q1Grade + q2Grade)/2;
-        }
+        qnumb++;
+        q1Grade += (int)(q1/qnumb);
+        q2Grade += (int)(q2/qnumb);
+        totalGrade = ((q1Grade + q2Grade)/2);
     }
     private int secretNumber;
     private String studentName;
     public int totalGrade;
     public int q1Grade = 0;
     public int q2Grade = 0;
-    private int q1numb = 0;
-    private int q2numb = 0;
+    private int qnumb = 0;
 }
